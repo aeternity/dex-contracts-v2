0a2b67af8c48022337943d3a605ed0f77dfd84e3
Bogdan Manole <bogdan.manole@frago.ro>
2021-12-06 13:19:36 +0200

    add errors.js to export folder

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,18 +1,24 @@
 const fs = require( 'fs' )
-var dir = __dirname + '/../build/'
+var exportDir = __dirname + '/../build/'
 
-if ( !fs.existsSync( dir ) ) {
-    fs.mkdirSync( dir )
+if ( !fs.existsSync( exportDir ) ) {
+    fs.mkdirSync( exportDir )
 }
 
 function exportContract ( fileName ) {
+
     const parent = __dirname + '/../contracts/interfaces/for-export/'
 
     const contract_content = fs.readFileSync( parent + fileName + ".aes", 'utf-8' )
-    const exportFileDest = dir + `/${fileName}.aes.js`
+    const exportFileDest = exportDir + `/${fileName}.aes.js`
     fs.writeFileSync( exportFileDest, `module.exports = \`\n${contract_content}\`;\n`, 'utf-8' )
     console.log( `'${exportFileDest}' written` )
 }
 
 exportContract( 'IAedexV2Router' )
 exportContract( 'IWAE' )
+
+//copy error
+const errorsFileDest = exportDir + `/errors.js`
+fs.copyFileSync( __dirname + "/../test/shared/utilities/errors.js", errorsFileDest )
+console.log( `'${errorsFileDest}' written` )
