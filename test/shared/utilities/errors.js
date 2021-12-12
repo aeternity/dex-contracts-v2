af48f3fa5df737883ce7da882d487bca5ae19930
Bogdan Manole <bogdan.manole@frago.ro>
2021-12-12 19:26:00 +0200

    refactor: additional verifications for burn_update

--- test/shared/utilities/errors.js
+++ test/shared/utilities/errors.js
@@ -41,4 +41,6 @@ module.exports = {
     "AedexV2Pair: INSUFFICIENT_LIQUIDITY_BURNED"     : "there is no liquidity to be burned for the given address",
     "AedexV2Pair: INSUFFICIENT_BALANCE_FOR_TRANSFER" : "insuficient balance for transfer",
     "AedexV2Pair: INSUFFICIENT_ALLOWANCE"            : "there is not enough allowance",
+    "AedexV2Pair: INSUFFICIENT_TOTAL_SUPPLY"         : "internal error: total supply is less than liquidity available for burning",
+    "AedexV2Pair: INSUFFICIENT_BALANCE_FOR_BURNING"  : "total supply is less than liquidity available for burning"
 }
